527839b4e12fdcb183578baf61bb62e2f738ced8
Javid Ahamed <104668160+ahamed-21@users.noreply.github.com>
2023-02-02 03:14:59 -0800

    Add files via upload

---
+++ ex3/color.js
@@ -0,0 +1,30 @@
+function black(){
+    document.getElementById('id1').style.color="#000000";
+}
+function blackout(){
+    document.getElementById('id1').style.color="#000000";
+}
+function blue(){
+    document.getElementById('id2').style.color="#0000ff";
+}
+function blueout(){
+    document.getElementById('id2').style.color="#000000";
+}
+function pink(){
+    document.getElementById('id3').style.color="#ff00ff";
+}
+function pinkout(){
+    document.getElementById('id3').style.color="#000000";
+}
+function gray(){
+    document.getElementById('id4').style.color="#808080";
+}
+function grayout(){
+    document.getElementById('id4').style.color="#000000";
+}
+function fun1(){
+    document.getElementById('id5').style.color="#008000";
+}
+function fun1out(){
+    document.getElementById('id5').style.color="#000000";
+}
